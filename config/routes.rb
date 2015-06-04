1e67f4e3094206b549c5115fe8c34cc65877681e
Bruno Soares <bruno@bsoares.com.br>
2015-06-04 18:00:46 -0300

    Configuring twitter-bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "categories#index"
   scope :admin, module: :admin, as: :admin do
     resources :categories, only: :index do
       resources :sub_categories, only: :index
