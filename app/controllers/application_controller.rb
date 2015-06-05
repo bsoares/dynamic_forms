3c42fd760723a4182d4b4a85bf9874b3131c20e3
Bruno Soares <bruno@bsoares.com.br>
2015-06-05 19:33:40 -0300

    Finally, dynamic form screen

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :load_categories_tree
+
+  def load_categories_tree
+    @categories_tree = Category.preload(:sub_categories)
+  end
 end
